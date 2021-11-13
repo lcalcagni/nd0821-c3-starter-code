5a3372daba3e216c79acfaed601776a1720b07d3
Laura Calcagni <l.calcagni@gmail.com>
2021-11-13 18:49:50 -0300

    Try path

--- starter/main.py
+++ starter/main.py
@@ -47,9 +47,9 @@ if "DYNO" in os.environ and os.path.isdir(".dvc"):
 
 app = FastAPI()
 
-model = pd.read_pickle(os.path.join(os.getcwd(), "starter/model/model.pkl"))
-Encoder = pd.read_pickle(os.path.join(os.getcwd(), "starter/model/encoder.pkl"))
-lb_ = pd.read_pickle(os.path.join(os.getcwd(),"starter/model/lb.pkl"))
+model = pd.read_pickle("./model/model.pkl")
+Encoder = pd.read_pickle( "./model/encoder.pkl")
+lb_ = pd.read_pickle("./model/lb.pkl")
 
 
 class Input(BaseModel):
