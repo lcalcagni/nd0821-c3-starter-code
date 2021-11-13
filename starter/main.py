9cd66daebc1e491f4f182e8c3bdbd4bf6e4e734d
Laura Calcagni <l.calcagni@gmail.com>
2021-11-13 17:34:22 -0300

    Fix dvc pull issue

--- starter/main.py
+++ starter/main.py
@@ -15,8 +15,8 @@ from model import inference
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
     os.system("dvc config core.hardlink_lock true")
-    # if os.system("dvc pull") != 0:
-    #     exit("dvc pull failed")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 app = FastAPI()
